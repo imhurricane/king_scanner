e0a1d08be09345761c74b8458fc384b2a4a096b9
imhurricane <imhurricane@163.com>
2021-03-22 14:48:00 +0800

    修改插件返回值。

--- android/src/main/java/com/comm/king_scanner/KingScannerPlugin.java
+++ android/src/main/java/com/comm/king_scanner/KingScannerPlugin.java
@@ -12,7 +12,6 @@ import androidx.core.content.ContextCompat;
 
 import com.comm.scan.CaptureActivity;
 
-import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
@@ -25,74 +24,74 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 public class KingScannerPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
 
-  private static final String CHANNEL = "scan_plugin";
-  private MethodChannel channel;
-  private Activity mActivity;
-  private Context mContext;
-  private Result mResult;
+    private static final String CHANNEL = "scan_plugin";
+    private MethodChannel channel;
+    private Activity mActivity;
+    private Context mContext;
+    private Result mResult;
 
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), CHANNEL);
-    channel.setMethodCallHandler(this);
-    mContext = flutterPluginBinding.getApplicationContext();
-  }
-
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), CHANNEL);
-    channel.setMethodCallHandler(new KingScannerPlugin());
-  }
-
-  @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    mResult = result;
-    if (call.method.equals("scan")) {
-      if (ContextCompat.checkSelfPermission(mActivity, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-        ActivityCompat.requestPermissions(mActivity, new String[]{Manifest.permission.CAMERA}, 1);
-      } else {
-        Intent intent = new Intent(mActivity, CaptureActivity.class);
-        mActivity.startActivityForResult(intent, 1000);
-//      result.success("Android " + android.os.Build.VERSION.RELEASE);
-      }
-    } else {
-      result.notImplemented();
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), CHANNEL);
+        channel.setMethodCallHandler(this);
+        mContext = flutterPluginBinding.getApplicationContext();
     }
-  }
 
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
-  }
+    public static void registerWith(Registrar registrar) {
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), CHANNEL);
+        channel.setMethodCallHandler(new KingScannerPlugin());
+    }
 
-  @Override
-  public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
-    mActivity = binding.getActivity();
-    binding.addActivityResultListener(new PluginRegistry.ActivityResultListener() {
-      @Override
-      public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 1000) {
-          final String string = data.getExtras().getString("SCAN_RESULT");
-          mResult.success(string);
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        mResult = result;
+        if (call.method.equals("scan")) {
+            if (ContextCompat.checkSelfPermission(mActivity, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(mActivity, new String[]{Manifest.permission.CAMERA}, 1);
+            } else {
+                Intent intent = new Intent(mActivity, CaptureActivity.class);
+                mActivity.startActivityForResult(intent, 1000);
+//                result.success("");
+            }
         } else {
-          mResult.notImplemented();
+            result.notImplemented();
         }
-        return false;
-      }
-    });
-  }
+    }
 
-  @Override
-  public void onDetachedFromActivityForConfigChanges() {
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+    }
 
-  }
+    @Override
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+        mActivity = binding.getActivity();
+        binding.addActivityResultListener(new PluginRegistry.ActivityResultListener() {
+            @Override
+            public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
+                if (requestCode == 1000 && data != null) {
+                    final String string = data.getExtras().getString("SCAN_RESULT");
+                    mResult.success(string);
+                } else {
+                    mResult.success("failed");
+                }
+                return false;
+            }
+        });
+    }
 
-  @Override
-  public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
 
-  }
+    }
 
-  @Override
-  public void onDetachedFromActivity() {
+    @Override
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
 
-  }
+    }
+
+    @Override
+    public void onDetachedFromActivity() {
+
+    }
 }
